1ea7951535765a767075871f39e19a9839eb48e8
Ammaar Bakshi <the.ammaar.ic@gmail.com>
2026-05-10 20:58:31 +0530

    Create ParityList.java

---
+++ ParityList.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class ParityList {
+
+    public static void main(String[] args){
+
+        Random r=new Random();
+
+        for(int i=0;i<10;i++){
+            int n=r.nextInt(100);
+            System.out.println(n+" -> "+(n%2==0?"Even":"Odd"));
+        }
+    }
+}
