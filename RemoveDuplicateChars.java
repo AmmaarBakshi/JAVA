0c55fbe6cf700fde6dffb208af95644eb5cdb13f
Ammaar Bakshi <the.ammaar.ic@gmail.com>
2026-05-25 23:50:07 +0530

    Create RemoveDuplicateChars.java

---
+++ RemoveDuplicateChars.java
@@ -0,0 +1,17 @@
+import java.util.stream.*;
+
+public class RemoveDuplicateChars {
+
+    public static void main(String[] args){
+
+        String s="banana";
+
+        String result =
+            s.chars()
+             .distinct()
+             .mapToObj(c->String.valueOf((char)c))
+             .collect(Collectors.joining());
+
+        System.out.println(result);
+    }
+}
