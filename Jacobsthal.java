a2c8c968b1b6625f89914a14eccf5fe8cfe0b6f7
Ammaar Bakshi <the.ammaar.ic@gmail.com>
2026-06-15 23:29:08 +0530

    Create Jacobsthal.java

---
+++ Jacobsthal.java
@@ -0,0 +1,18 @@
+import java.util.stream.*;
+
+public class Jacobsthal {
+
+    public static void main(String[] args){
+
+        Stream.iterate(
+                new long[]{0,1},
+                a -> new long[]{
+                    a[1],
+                    a[1] + 2*a[0]
+                }
+        )
+        .limit(20)
+        .map(a -> a[0])
+        .forEach(System.out::println);
+    }
+}
